a998cd03afef4299eabd39703a8ddd9645f38e55
ArjobanSingh <arjobansingh1998@gmail.com>
2024-07-08 15:13:52 +0530

    add imperative handle to key press item

--- src/components/ReactKeyPress/KeyPressItem.jsx
+++ src/components/ReactKeyPress/KeyPressItem.jsx
@@ -1,13 +1,21 @@
-import { useEffect, useId } from "react";
+import { useEffect, useId, forwardRef, useImperativeHandle } from "react";
 import { useLatestValue } from "../../hooks/useLatestValue";
 import { useKeyPressValue } from "./KeyPressProvider";
 
-export default function KeyPressItem({ children, name, keyCombo, onKeyEvent }) {
+const KeyPressItem = forwardRef(function KeyPressItem(
+  { children, name, keyCombo, onKeyEvent },
+  ref
+) {
   const itemId = useId();
 
   // This returns a stable ref object, with current being latest fn reference
   const onKeyEventRef = useLatestValue(onKeyEvent);
   const { registerChildren, unregisterChildren } = useKeyPressValue();
+
+  // in case the callee wants to unregister event programmatically
+  useImperativeHandle(ref, () => ({
+    unregisterEvent: () => unregisterChildren(itemId),
+  }));
 
   // only on mount, unmount
   useEffect(() => {
@@ -23,13 +31,16 @@ export default function KeyPressItem({ children, name, keyCombo, onKeyEvent }) {
       unregisterChildren(itemId);
     };
   }, [
-    itemId,
     keyCombo,
     name,
+    // following won't change in lifecycle of item, adding for best practice
+    itemId,
     onKeyEventRef,
     registerChildren,
     unregisterChildren,
   ]);
 
   return children;
-}
+});
+
+export default KeyPressItem;
